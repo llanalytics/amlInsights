1330ace53f826e9fd9ab157ba555ac5b7883ad4b
Eric Hale <eric.hale@longleaf-analytics.com>
2026-03-31 10:13:42 -0400

    heroku cleanup

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from fastapi import FastAPI
-from mangum import Mangum
 
 app = FastAPI()
 
 
 @app.get("/")
 def hello_world() -> dict[str, str]:
-    return {"message": "Hello, World from FastAPI on AWS Lambda!"}
+    return {"message": "Hello, World from FastAPI on Heroku!"}
 
 
 @app.get("/health")
 def health() -> dict[str, str]:
     return {"status": "ok"}
-
-
-handler = Mangum(app)
